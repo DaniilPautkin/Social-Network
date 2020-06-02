04e0d915d847ffe510062e4f02110fde0fea004f
Daniil Pautkin <pautkin2003@gmail.com>
2020-06-02 13:33:36 +0300

    Updates

---
+++ precache-manifest.ca9ae2ed9e6b406e7b36363c5d56ca7a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4a550285b9dfbc77623",
+    "url": "/Social-Network/static/js/2.b4a55028.chunk.js"
+  },
+  {
+    "revision": "7c9ce5cd53b32acf6287",
+    "url": "/Social-Network/static/css/main.62e68277.chunk.css"
+  },
+  {
+    "revision": "1701e2f46690453a4402316b232bb992",
+    "url": "/Social-Network/static/media/giphy.1701e2f4.gif"
+  },
+  {
+    "revision": "f60f973f59806d893994",
+    "url": "/Social-Network/static/js/1.f60f973f.chunk.js"
+  },
+  {
+    "revision": "d6759143baffd6711527ab1dcffcf093",
+    "url": "/Social-Network/static/media/USER.d6759143.png"
+  },
+  {
+    "revision": "7c9ce5cd53b32acf6287",
+    "url": "/Social-Network/static/js/main.7c9ce5cd.chunk.js"
+  },
+  {
+    "revision": "5ecf3d7707988b109acd",
+    "url": "/Social-Network/static/js/3.5ecf3d77.chunk.js"
+  },
+  {
+    "revision": "1b1c5109cd7cdd1bcb91",
+    "url": "/Social-Network/static/js/runtime~main.1b1c5109.js"
+  },
+  {
+    "revision": "b4a550285b9dfbc77623",
+    "url": "/Social-Network/static/css/2.77dacf8d.chunk.css"
+  },
+  {
+    "revision": "f60f973f59806d893994",
+    "url": "/Social-Network/static/css/1.1def69a9.chunk.css"
+  },
+  {
+    "revision": "02cb2a8eb8a71e1a01e97f9e7a3ecf5c",
+    "url": "/Social-Network/index.html"
+  }
+];
